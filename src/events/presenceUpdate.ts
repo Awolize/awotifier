7cd6115a382d06b50ed8ee7cc92e258de8d2dd28
Awolize <alex.store@hotmail.com>
2023-12-04 18:50:32 +0100

    added biomejs

--- src/events/presenceUpdate.ts
+++ src/events/presenceUpdate.ts
@@ -1,56 +1,84 @@
-import { ActivityType, Client, Message, Presence, TextChannel } from "discord.js";
+import {
+	ActivityType,
+	Client,
+	Message,
+	Presence,
+	TextChannel,
+} from "discord.js";
 import { GUILD_ID, TEXT_CHANNEL_ID } from "../config";
 
 interface MemberActivities {
-    [userId: string]: ActivityType;
+	[userId: string]: ActivityType;
 }
 
 interface GoneLiveMessages {
-    [userId: string]: Message;
+	[userId: string]: Message;
 }
 
 const memberActivities: MemberActivities = {};
 const goneLiveMessages: GoneLiveMessages = {};
 
-export const printOnLive = async (client: Client, oldPresence: Presence, presence: Presence): Promise<void> => {
-    try {
-        const { user, guild, activities } = presence;
-        const textChannel = client.channels.cache.get(TEXT_CHANNEL_ID) as TextChannel;
+export const printOnLive = async (
+	client: Client,
+	oldPresence: Presence,
+	presence: Presence,
+): Promise<void> => {
+	try {
+		const { user, guild, activities } = presence;
+		const textChannel = client.channels.cache.get(
+			TEXT_CHANNEL_ID,
+		) as TextChannel;
 
-        if (!user || !guild) {
-            console.log("no user or guild?", user, guild);
-            
-            return;}
+		if (!user || !guild) {
+			console.log("no user or guild?", user, guild);
 
-        // only run once no matter the server, The Cave
-        if (guild.id !== GUILD_ID) {
-            return;
-        }
+			return;
+		}
 
-        const isStreaming = activities?.some((activity) => activity.type === ActivityType.Streaming);
-        const wasStreaming = oldPresence?.activities?.some((activity) => activity.type === ActivityType.Streaming);
+		// only run once no matter the server, The Cave
+		if (guild.id !== GUILD_ID) {
+			return;
+		}
 
-        // If the user just started streaming, send a message in the text channel.
-        if (isStreaming && !wasStreaming) {
-            memberActivities[user.id] = ActivityType.Streaming;
-            const activity = activities?.find((activity) => activity.type === ActivityType.Streaming);
+		const isStreaming = activities?.some(
+			(activity) => activity.type === ActivityType.Streaming,
+		);
+		const wasStreaming = oldPresence?.activities?.some(
+			(activity) => activity.type === ActivityType.Streaming,
+		);
 
-            if (activity) {
-                goneLiveMessages[user.id] = await textChannel.send(
-                    `${user.toString()} is now live! <t:${Math.round(new Date().getTime() / 1000)}>\n\n${activity?.details}\n**in** ${activity?.state}\n**at** ${activity?.url}`
-                );
-                console.log(`${user.tag} is now live in ${activity?.state}`);
-            }
-        }
+		// If the user just started streaming, send a message in the text channel.
+		if (isStreaming && !wasStreaming) {
+			memberActivities[user.id] = ActivityType.Streaming;
+			const activity = activities?.find(
+				(activity) => activity.type === ActivityType.Streaming,
+			);
 
-        // If the user stopped streaming, update the message in the text channel.
-        if (!isStreaming && wasStreaming) {
-            const message = goneLiveMessages[user.id]
-            if (message) {
-                message.edit(message.content + `\n\n**Stream ended at <t:${Math.round(new Date().getTime() / 1000)}>**`);
-            }
-        }
-    } catch (error) {
-        console.error(error);
-    }
+			if (activity) {
+				goneLiveMessages[user.id] = await textChannel.send(
+					`${user.toString()} is now live! <t:${Math.round(
+						new Date().getTime() / 1000,
+					)}>\n\n${activity?.details}\n**in** ${activity?.state}\n**at** ${
+						activity?.url
+					}`,
+				);
+				console.log(`${user.tag} is now live in ${activity?.state}`);
+			}
+		}
+
+		// If the user stopped streaming, update the message in the text channel.
+		if (!isStreaming && wasStreaming) {
+			const message = goneLiveMessages[user.id];
+			if (message) {
+				message.edit(
+					message.content +
+						`\n\n**Stream ended at <t:${Math.round(
+							new Date().getTime() / 1000,
+						)}>**`,
+				);
+			}
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,15 @@ import { Client, Events, GatewayIntentBits, Presence } from "discord.js";
 import { printOnLive } from "./events/presenceUpdate";
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences],
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences],
 });
 
 client.once(Events.ClientReady, (c) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on(Events.PresenceUpdate, async (oldEvent, event) => printOnLive(client, oldEvent as Presence, event as Presence));
+client.on(Events.PresenceUpdate, async (oldEvent, event) =>
+	printOnLive(client, oldEvent as Presence, event as Presence),
+);
 
 client.login(process.env.TOKEN);
